b5fe99d5fb6eca05788b458c8a8f2bf22594d096
Susie Son <susieson99@gmail.com>
2024-07-26 20:59:38 -0400

    Remove unused import

--- app/src/main/java/com/example/mywikisearcher/ui/components/ArticleList.kt
+++ app/src/main/java/com/example/mywikisearcher/ui/components/ArticleList.kt
@@ -1,7 +1,6 @@
 package com.example.mywikisearcher.ui.components
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
